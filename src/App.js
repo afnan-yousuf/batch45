bf850c6813b03f67bca80fc3720601c68b8f7def
afnan-yousuf <afnanyousuf@gmail.com>
2025-04-29 12:08:23 +0500

    css module working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import style from './App.module.css'
+import Afnan from './components/Afnan'
 
 function App() {
   return (
     <>
       <h1>Afnan Yousuf</h1>
+      <p>
+        This is paragraph
+      </p>
+      <h1 className={style.myheading}>Testing</h1>
+      <button className={style.button}>OK</button>
+      <Afnan />
     </>
   );
 }
---
+++ src/components/Afnan.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from './Afnan.module.css'
+
+
+function Afnan() {
+  return (
+    <div>
+        <h1 className={style.myheading}>Afnan Yousuf From Component</h1>
+        <button className={style.button}>OK</button>
+    </div>
+  )
+}
+
+export default Afnan
